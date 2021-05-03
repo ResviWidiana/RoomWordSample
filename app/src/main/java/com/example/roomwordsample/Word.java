ffb28be106d91f7687adf7987d2f66526bf1cfbc
ResviWidiana <resvi.widiana.tif19@polban.ac.id>
2021-05-03 10:14:11 +0700

    initial commit

---
+++ app/src/main/java/com/example/roomwordsample/Word.java
@@ -0,0 +1,16 @@
+package com.example.roomwordsample;
+
+import androidx.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
